ac9d10926553823a16f02c5dac4ad27e2550f5e3
IGOR ROCHA DA SILVA <igor082011@gmail.com>
2022-10-26 17:40:31 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import express from "express";
+
+import { usersRoutes } from "./routes/users.routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/users", usersRoutes);
+
+export { app };
---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,27 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user.admin) {
+      throw new Error("User is not a admin");
+    }
+
+    if (!user) {
+      throw new Error("User no exist");
+    }
+
+    const users = this.usersRepository.list();
+    return users;
+  }
+}
+
+export { ListAllUsersUseCase };
